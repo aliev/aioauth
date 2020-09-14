eeab5cb61b45e5c9e7fe37a3f45c8e21b24a7e80
Ali Aliev <shamkir@gmail.com>
2020-09-14 11:33:22 +0400

    Added implicit flow test

--- examples/fastapi/tests/test_oauth2.py
+++ examples/fastapi/tests/test_oauth2.py
@@ -16,6 +16,33 @@ async def test_auth(client: TestClient, client_record: ClientTable):
 
 
 @pytest.mark.asyncio
+async def test_implicit_flow(client: TestClient, client_record: ClientTable):
+    query = {
+        "client_id": client_record.client_id,
+        "redirect_uri": client_record.client_metadata["redirect_uris"][0],
+        "response_type": ResponseType.TYPE_TOKEN.value,
+        "state": "some random state",
+        "scope": client_record.client_metadata["scope"],
+    }
+    response = await client.get("/oauth/v2/authorize", query_string=query)
+    assert response.status_code == status.HTTP_200_OK
+
+    data = {
+        "username": "admin",
+        "password": "admin",
+    }
+
+    # Get authorization code
+    response = await client.post(
+        "/oauth/v2/authorize", query_string=query, form=data, allow_redirects=False
+    )
+    assert response.status_code == status.HTTP_302_FOUND
+    redirect_uri = urlparse(response.headers["location"])
+    fragment = dict(parse_qsl(redirect_uri.fragment))
+    assert "access_token" in fragment
+
+
+@pytest.mark.asyncio
 async def test_authorization_code_flow(client: TestClient, client_record: ClientTable):
     # Check get method
     query = {
