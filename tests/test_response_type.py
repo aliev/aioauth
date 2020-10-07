4be252b257fa4ba0ef98e06e7eed6b29662a7966
Ali Aliev <shamkir@gmail.com>
2020-10-07 19:19:17 +0400

    Tests cleanup

--- tests/test_response_type.py
+++ tests/test_response_type.py
@@ -1,103 +1,123 @@
 from http import HTTPStatus
-from typing import Dict
 
 import pytest
 from async_oauth2_provider.endpoints import OAuth2Endpoint
 from async_oauth2_provider.requests import Query, Request
 from async_oauth2_provider.types import CodeChallengeMethod, RequestMethod, ResponseType
 from async_oauth2_provider.utils import generate_token
-from tests.conftest import Defaults
+
+from .conftest import Defaults
 
 
 @pytest.mark.asyncio
-async def test_anonymous_user(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_anonymous_user(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    client_id = defaults.client_id
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    request_url = "https://localhost"
 
     query = Query(
-        client_id=defaults.client_id,
+        client_id=client_id,
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
+        redirect_uri=redirect_uri,
+        scope=scope,
         state=generate_token(10),
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
-    request = Request(url="https://localhost", query=query, method=RequestMethod.GET,)
+    request = Request(url=request_url, query=query, method=RequestMethod.GET,)
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_invalid_response_type(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_invalid_response_type(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    client_id = defaults.client_id
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    request_url = "https://localhost"
+    user = "username"
+    response_type = "invalid"
 
     query = Query(
-        client_id=defaults.client_id,
-        response_type="Invalid",  # type: ignore
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
+        client_id=client_id,
+        response_type=response_type,  # type: ignore
+        redirect_uri=redirect_uri,
+        scope=scope,
         state=generate_token(10),
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
-    )
-    response = await endpoint.create_authorization_code_response(request)
-    assert response.status_code == HTTPStatus.BAD_REQUEST
-
-    query = Query(
-        client_id=defaults.client_id,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
-        code_challenge_method=CodeChallengeMethod.PLAIN,
-        code_challenge=code_challenge,
-    )
-
-    request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_empty_client_id(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_empty_client_id(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    request_url = "https://localhost"
+    user = "username"
 
     query = Query(
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
+        redirect_uri=redirect_uri,
+        scope=scope,
         state=generate_token(10),
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_empty_redirect_uri(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_empty_redirect_uri(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    client_id = defaults.client_id
+    scope = defaults.scope
+    request_url = "https://localhost"
+    user = "username"
 
     query = Query(
-        client_id=defaults.client_id,
+        client_id=client_id,
         response_type=ResponseType.TYPE_CODE,
+        scope=scope,
+        state=generate_token(10),
+        code_challenge_method=CodeChallengeMethod.PLAIN,
+        code_challenge=code_challenge,
+    )
+
+    request = Request(
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
+    )
+    response = await endpoint.create_authorization_code_response(request)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@pytest.mark.asyncio
+async def test_invalid_client_id(endpoint: OAuth2Endpoint, defaults: Defaults):
+    code_challenge = generate_token(128)
+    redirect_uri = defaults.redirect_uri
+    client_id = "invalid"
+    request_url = "https://localhost"
+    user = "username"
+
+    query = Query(
+        client_id=client_id,
+        response_type=ResponseType.TYPE_CODE,
+        redirect_uri=redirect_uri,
         scope=defaults.scope,
         state=generate_token(10),
         code_challenge_method=CodeChallengeMethod.PLAIN,
@@ -105,79 +125,59 @@ async def test_empty_redirect_uri(
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_invalid_client_id(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_insecure_transport(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    request_url = "https://localhost"
+    user = "username"
 
     query = Query(
         client_id="invalid",
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
+        redirect_uri=redirect_uri,
+        scope=scope,
         state=generate_token(10),
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_insecure_transport(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_allowed_method(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    client_id = defaults.client_id
+    request_url = "https://localhost"
+    user = "username"
+    state = generate_token(10)
+    scope = defaults.scope
+    redirect_uri = defaults.redirect_uri
 
     query = Query(
-        client_id="invalid",
+        client_id=client_id,
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
+        redirect_uri=redirect_uri,
+        scope=scope,
+        state=state,
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="http://localhost", query=query, method=RequestMethod.GET, user="username",
-    )
-    response = await endpoint.create_authorization_code_response(request)
-    assert response.status_code == HTTPStatus.BAD_REQUEST
-
-
-@pytest.mark.asyncio
-async def test_allowed_method(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
-    code_challenge = generate_token(128)
-
-    query = Query(
-        client_id="invalid",
-        response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
-        code_challenge_method=CodeChallengeMethod.PLAIN,
-        code_challenge=code_challenge,
-    )
-
-    request = Request(
-        url="https://localhost",
-        query=query,
-        method=RequestMethod.POST,
-        user="username",
+        url=request_url, query=query, method=RequestMethod.POST, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
@@ -186,64 +186,78 @@ async def test_allowed_method(
 
 @pytest.mark.asyncio
 async def test_invalid_code_challange_method(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
+    endpoint: OAuth2Endpoint, defaults: Defaults
 ):
+    state = generate_token(10)
     code_challenge = generate_token(128)
+    code_challenge_method = "invalid"
+    scope = defaults.scope
+    redirect_uri = defaults.redirect_uri
+    request_url = "https://localhost"
 
     query = Query(
         client_id=defaults.client_id,
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
-        code_challenge_method="invalid",
+        redirect_uri=redirect_uri,
+        scope=scope,
+        state=state,
+        code_challenge_method=code_challenge_method,  # type: ignore
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user="username",
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_empty_code_challange(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_empty_code_challange(endpoint: OAuth2Endpoint, defaults: Defaults):
+    client_id = defaults.client_id
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    state = generate_token(10)
+    request_url = "https://localhost"
+    user = "username"
+
     query = Query(
-        client_id=defaults.client_id,
+        client_id=client_id,
         response_type=ResponseType.TYPE_CODE,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
+        redirect_uri=redirect_uri,
+        scope=scope,
+        state=state,
         code_challenge_method=CodeChallengeMethod.PLAIN,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.asyncio
-async def test_empty_response_type(
-    endpoint: OAuth2Endpoint, defaults: Defaults, storage: Dict
-):
+async def test_empty_response_type(endpoint: OAuth2Endpoint, defaults: Defaults):
     code_challenge = generate_token(128)
+    client_id = defaults.client_id
+    redirect_uri = defaults.redirect_uri
+    scope = defaults.scope
+    request_url = "https://localhost"
+    user = "username"
+    state = generate_token(10)
 
     query = Query(
-        client_id=defaults.client_id,
-        redirect_uri=defaults.redirect_uri,
-        scope=defaults.scope,
-        state=generate_token(10),
+        client_id=client_id,
+        redirect_uri=redirect_uri,
+        scope=scope,
+        state=state,
         code_challenge_method=CodeChallengeMethod.PLAIN,
         code_challenge=code_challenge,
     )
 
     request = Request(
-        url="https://localhost", query=query, method=RequestMethod.GET, user="username",
+        url=request_url, query=query, method=RequestMethod.GET, user=user,
     )
     response = await endpoint.create_authorization_code_response(request)
     assert response.status_code == HTTPStatus.BAD_REQUEST
