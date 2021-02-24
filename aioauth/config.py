d084be46616053a8dac84eae95823789bcfd0d12
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2021-02-24 18:54:56 -0500

    Updated config.py documentation.

--- aioauth/config.py
+++ aioauth/config.py
@@ -12,18 +12,19 @@ from types import SimpleNamespace
 class Settings(SimpleNamespace):
     """Configuration options that is used by the Server class."""
 
-    TOKEN_EXPIRES_IN: int = 86400
-    """Access token lifetime in seconds."""
+    TOKEN_EXPIRES_IN: int = 24 * 60 * 60
+    """Access token lifetime in seconds. Defaults to 24 hours."""
 
-    AUTHORIZATION_CODE_EXPIRES_IN: int = 300
-    """Authorization code lifetime in seconds."""
+    AUTHORIZATION_CODE_EXPIRES_IN: int = 5 * 60
+    """Authorization code lifetime in seconds. Defaults to 5 minutes."""
 
     INSECURE_TRANSPORT: bool = False
     """Allow connections over SSL only.
 
     Note:
-        When this option is disabled server will raise"HTTP method is not allowed"error when
-        attempting to access the server without a valid SSL tunnel.
+        When this option is disabled server will raise "HTTP method is
+        not allowed" error when attempting to access the server without
+        a valid SSL tunnel.
     """
 
     ERROR_URI: str = ""
