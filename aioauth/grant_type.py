9592d3ed201ee67de9e4c8b4e3af4a333b180d78
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2021-02-24 18:56:37 -0500

    grant_type.py documentation.

--- aioauth/grant_type.py
+++ aioauth/grant_type.py
@@ -1,3 +1,9 @@
+"""
+.. code-block:: python
+
+    from aioauth import grant_type
+"""
+
 from typing import Optional
 
 from .base.request_validator import BaseRequestValidator
@@ -17,16 +23,51 @@ from .utils import decode_auth_headers, list_to_scope, scope_to_list
 
 
 class GrantTypeBase(BaseRequestValidator):
-    allowed_methods = [
-        RequestMethod.POST,
-    ]
+    """Base grant type that all other grant types inherit from."""
+
+    allowed_methods = [RequestMethod.POST]
     grant_type: Optional[GrantType] = None
 
+    async def validate_request(self, request: Request) -> Client:
+        """Validates the client request to ensure it is valid."""
+
+        await super().validate_request(request)
+
+        client_id, client_secret = decode_auth_headers(request)
+
+        client = await self.db.get_client(
+            request,
+            client_id=client_id,
+            client_secret=client_secret,
+        )
+
+        if not client:
+            raise InvalidRequestError(
+                request=request,
+                description="Invalid client_id parameter value.",
+            )
+        elif not request.post.grant_type:
+            raise InvalidRequestError(
+                request=request,
+                description="Request is missing grant type.",
+            )
+        elif self.grant_type != request.post.grant_type:
+            raise UnsupportedGrantTypeError(request=request)
+        elif not client.check_grant_type(request.post.grant_type):
+            raise UnauthorizedClientError(request=request)
+        elif not client.check_scope(request.post.scope):
+            raise InvalidScopeError(request=request)
+
+        return client
+
     async def create_token_response(self, request: Request) -> TokenResponse:
-        """ Validate token request and create token response. """
+        """Creates token response to reply to client."""
+
         client = await self.validate_request(request)
         token = await self.db.create_token(
-            request, client.client_id, request.post.scope
+            request,
+            client.client_id,
+            request.post.scope,
         )
 
         return TokenResponse(
@@ -38,38 +79,23 @@ class GrantTypeBase(BaseRequestValidator):
             token_type=token.token_type,
         )
 
-    async def validate_request(self, request: Request) -> Client:
-        await super().validate_request(request)
-
-        client_id, client_secret = decode_auth_headers(request)
-
-        client = await self.db.get_client(
-            request, client_id=client_id, client_secret=client_secret
-        )
-
-        if not client:
-            raise InvalidRequestError(
-                request=request, description="Invalid client_id parameter value."
-            )
-
-        if not request.post.grant_type:
-            raise InvalidRequestError(
-                request=request, description="Request is missing grant type."
-            )
-
-        if self.grant_type != request.post.grant_type:
-            raise UnsupportedGrantTypeError(request=request)
-
-        if not client.check_grant_type(request.post.grant_type):
-            raise UnauthorizedClientError(request=request)
-
-        if not client.check_scope(request.post.scope):
-            raise InvalidScopeError(request=request)
-
-        return client
-
 
 class AuthorizationCodeGrantType(GrantTypeBase):
+    """
+    The Authorization Code grant type is used by confidential and public
+    clients to exchange an authorization code for an access token. After
+    the user returns to the client via the redirect URL, the application
+    will get the authorization code from the URL and use it to request
+    an access token.
+
+    It is recommended that all clients use `RFC 7636 <https://tools.ietf.org/html/rfc7636>`_
+    Proof Key for Code Exchange extension with this flow as well to
+    provide better security. Note that ``aioauth`` implements RFC 7636
+    out-of-the-box.
+
+    See `RFC 6749 section 1.3.1 <https://tools.ietf.org/html/rfc6749#section-1.3.1>`_.
+    """
+
     grant_type: GrantType = GrantType.TYPE_AUTHORIZATION_CODE
 
     async def validate_request(self, request: Request) -> Client:
@@ -77,21 +103,24 @@ class AuthorizationCodeGrantType(GrantTypeBase):
 
         if not request.post.redirect_uri:
             raise InvalidRequestError(
-                request=request, description="Mismatching redirect URI."
+                request=request,
+                description="Mismatching redirect URI.",
             )
-
-        if not client.check_redirect_uri(request.post.redirect_uri):
+        elif not client.check_redirect_uri(request.post.redirect_uri):
             raise InvalidRequestError(
-                request=request, description="Invalid redirect URI."
+                request=request,
+                description="Invalid redirect URI.",
             )
-
-        if not request.post.code:
+        elif not request.post.code:
             raise InvalidRequestError(
-                request=request, description="Missing code parameter."
+                request=request,
+                description="Missing code parameter.",
             )
 
         authorization_code = await self.db.get_authorization_code(
-            request, client.client_id, request.post.code
+            request,
+            client.client_id,
+            request.post.code,
         )
 
         if not authorization_code:
@@ -123,6 +152,18 @@ class AuthorizationCodeGrantType(GrantTypeBase):
 
 
 class PasswordGrantType(GrantTypeBase):
+    """
+    The Password grant type is a way to exchange a user's credentials
+    for an access token. Because the client application has to collect
+    the user's password and send it to the authorization server, it is
+    not recommended that this grant be used at all anymore.
+
+    See `RFC 6749 section 1.3.3 <https://tools.ietf.org/html/rfc6749#section-1.3.3>`_.
+
+    The latest `OAuth 2.0 Security Best Current Practice <https://tools.ietf.org/html/draft-ietf-oauth-security-topics-13#section-3.4>`_
+    disallows the password grant entirely.
+    """
+
     grant_type: GrantType = GrantType.TYPE_PASSWORD
 
     async def validate_request(self, request: Request) -> Client:
@@ -144,10 +185,29 @@ class PasswordGrantType(GrantTypeBase):
 
 
 class RefreshTokenGrantType(GrantTypeBase):
+    """
+    The Refresh Token grant type is used by clients to exchange a
+    refresh token for an access token when the access token has expired.
+    This allows clients to continue to have a valid access token without
+    further interaction with the user.
+
+    See `RFC 6749 section 1.5 <https://tools.ietf.org/html/rfc6749#section-1.5>`_.
+    """
+
     grant_type: GrantType = GrantType.TYPE_REFRESH_TOKEN
 
+    async def validate_request(self, request: Request) -> Client:
+        client = await super().validate_request(request)
+
+        if not request.post.refresh_token:
+            raise InvalidRequestError(
+                request=request,
+                description="Missing refresh token parameter.",
+            )
+
+        return client
+
     async def create_token_response(self, request: Request) -> TokenResponse:
-        """ Validate token request and create token response. """
         client = await self.validate_request(request)
 
         old_token = await self.db.get_token(
@@ -156,7 +216,11 @@ class RefreshTokenGrantType(GrantTypeBase):
             refresh_token=request.post.refresh_token,
         )
 
-        if not old_token or old_token.revoked or old_token.refresh_token_expired:
+        if (
+            not old_token
+            or old_token.revoked
+            or old_token.refresh_token_expired
+        ):
             raise InvalidGrantError(request=request)
 
         # Revoke old token
@@ -176,7 +240,11 @@ class RefreshTokenGrantType(GrantTypeBase):
                 )
             )
 
-        token = await self.db.create_token(request, client.client_id, new_scope)
+        token = await self.db.create_token(
+            request,
+            client.client_id,
+            new_scope,
+        )
 
         return TokenResponse(
             expires_in=token.expires_in,
@@ -187,16 +255,15 @@ class RefreshTokenGrantType(GrantTypeBase):
             token_type=token.token_type,
         )
 
-    async def validate_request(self, request: Request) -> Client:
-        client = await super().validate_request(request)
-
-        if not request.post.refresh_token:
-            raise InvalidRequestError(
-                request=request, description="Missing refresh token parameter."
-            )
-
-        return client
-
 
 class ClientCredentialsGrantType(GrantTypeBase):
+    """
+    The Client Credentials grant type is used by clients to obtain an
+    access token outside of the context of a user. This is typically
+    used by clients to access resources about themselves rather than to
+    access a user's resources.
+
+    See `RFC 6749 section 4.4 <https://tools.ietf.org/html/rfc6749#section-4.4>`_.
+    """
+
     grant_type: GrantType = GrantType.TYPE_CLIENT_CREDENTIALS
