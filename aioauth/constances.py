dcffe52a36adc2610c14307259146f8114922db5
Ali Aliev <shamkir@gmail.com>
2021-08-09 20:24:23 +0200

    constances.py documentation

--- aioauth/constances.py
+++ aioauth/constances.py
@@ -1,12 +1,12 @@
+"""
+.. code-block:: python
+    from aioauth import constances
+Constants that are used throughout the project.
+----
+"""
+
 from .structures import CaseInsensitiveDict
 
-"""
-The authorization server MUST include the HTTP "Cache-Control"
-response header field [RFC2616] with a value of "no-store" in any
-response containing tokens, credentials, or other sensitive
-information, as well as the "Pragma" response header field [RFC2616]
-with a value of "no-cache".
-"""
 default_headers = CaseInsensitiveDict(
     {
         "Content-Type": "application/json",
@@ -14,3 +14,10 @@ default_headers = CaseInsensitiveDict(
         "Pragma": "no-cache",
     }
 )
+"""
+The authorization server **must** include the HTTP ``Cache-Control``
+response header field, as per RFC2616, with a value of ``no-store`` in
+any response containing tokens, credentials, or other sensitive
+information, as well as the ``Pragma`` response header field, as per
+RFC2616, with a value of ``no-cache``.
+"""
