9cad3ddd6d3574e9cb42483edac277cca3d16ec1
Ali Aliev <shamkir@gmail.com>
2021-08-09 20:47:33 +0200

    Added config.py documentation

--- aioauth/config.py
+++ aioauth/config.py
@@ -1,9 +1,32 @@
+"""
+.. code-block:: python
+    from aioauth import config
+Configuration settings for aioauth server instance.
+----
+"""
+
 from typing import NamedTuple
 
 
 class Settings(NamedTuple):
-    TOKEN_EXPIRES_IN: int = 86400
-    AUTHORIZATION_CODE_EXPIRES_IN: int = 300
+    """Configuration options that is used by the Server class."""
+
+    TOKEN_EXPIRES_IN: int = 24 * 60 * 60
+    """Access token lifetime in seconds. Defaults to 24 hours."""
+
+    AUTHORIZATION_CODE_EXPIRES_IN: int = 5 * 60
+    """Authorization code lifetime in seconds. Defaults to 5 minutes."""
+
     INSECURE_TRANSPORT: bool = False
+    """Allow connections over SSL only.
+    Note:
+        When this option is disabled server will raise "HTTP method is
+        not allowed" error when attempting to access the server without
+        a valid SSL tunnel.
+    """
+
     ERROR_URI: str = ""
+    """URI to redirect resource owner when server encounters error."""
+
     AVAILABLE: bool = True
+    """Boolean indicating whether or not the server is available."""
