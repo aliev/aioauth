24ec2748d9311576a1204df1eee717c36e715434
Danny Guinther <dguinther@neuralmagic.com>
2023-03-30 06:19:55 -0400

    Add some docs for OIDC Request and prompt param

--- aioauth/oidc/core/requests.py
+++ aioauth/oidc/core/requests.py
@@ -7,12 +7,21 @@ from aioauth.requests import BaseRequest, Query as BaseQuery, Post
 
 @dataclass
 class Query(BaseQuery):
+    # Space delimited, case sensitive list of ASCII string values that
+    # specifies whether the Authorization Server prompts the End-User for
+    # reauthentication and consent. The defined values are: none, login,
+    # consent, select_account.
+    # https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
     prompt: Optional[str] = None
 
 
 @dataclass
 class Request(BaseRequest[Query, Post, Any]):
-    """Object that contains a client's complete request."""
+    """
+    Object that contains a client's complete request with extensions as defined
+    by OpenID Core.
+    https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+    """
 
     query: Query = field(default_factory=Query)
     post: Post = field(default_factory=Post)
