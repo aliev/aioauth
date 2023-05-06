f3a79d22d01b00e0d382c2747a87ec30ff2c7664
GitHub Action <action@github.com>
2023-05-06 22:39:56 +0000

    Documentation update.

--- aioauth/oidc/core/responses.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-from aioauth.responses import TokenResponse as OAuthTokenResponse
-
-
-@dataclass
-class TokenResponse(OAuthTokenResponse):
-    id_token: Optional[str] = None
