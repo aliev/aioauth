71f8d6af40e9f174c5031f7516ff7f482fdfc77e
imgurbot12 <imgurbot12@gmail.com>
2024-11-25 15:20:54 -0700

    feat: formatting example and minor tweaks

--- examples/fastapi/example.py
+++ examples/fastapi/example.py
@@ -3,6 +3,17 @@ Bare Minimum Example of FastAPI Implementation of AioAuth
 
 (Supports AuthCode/Token/RefreshToken ONLY)
 """
+
+import os
+import sys
+
+BASE_DIR = os.path.dirname(__file__)
+EXAMPLES_DIR = os.path.relpath(os.path.join(BASE_DIR, "../"))
+AIOAUTH_DIR = os.path.relpath(os.path.join(BASE_DIR, "../../"))
+
+sys.path.insert(0, EXAMPLES_DIR)  # to import `aioauth/shared`
+sys.path.insert(0, AIOAUTH_DIR)  # to import `aioauth/aioauth`
+
 import json
 from http import HTTPStatus
 from typing import cast
@@ -13,18 +24,17 @@ from fastapi_extras.session import SessionMiddleware
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from aioauth.collections import HTTPHeaderDict
-from aioauth.config import Settings
 from aioauth.requests import Post, Query
 from aioauth.requests import Request as OAuthRequest
 from aioauth.responses import Response as OAuthResponse
 from aioauth.types import RequestMethod
 
-from store import AuthServer, BackendStore, engine, auto_login, lifespan
+from shared import AuthServer, BackendStore, engine, settings, auto_login, lifespan
 
-app      = FastAPI(lifespan=lifespan)
-settings = Settings(INSECURE_TRANSPORT=True)
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(SessionMiddleware)
+
 
 async def get_auth_server() -> AuthServer:
     """
@@ -34,21 +44,23 @@ async def get_auth_server() -> AuthServer:
     storage = BackendStore(session)
     return AuthServer(storage)
 
+
 async def to_request(request: Request) -> OAuthRequest:
     """
     convert fastapi request to aioauth oauth2 request
     """
-    user = request.session.get('user', None)
+    user = request.session.get("user", None)
     form = await request.form()
     return OAuthRequest(
         headers=HTTPHeaderDict(**request.headers),
         method=cast(RequestMethod, request.method),
-        post=Post(**form), #type: ignore
-        query=Query(**request.query_params), #type: ignore
+        post=Post(**form),  # type: ignore
+        query=Query(**request.query_params),  # type: ignore
         settings=settings,
         url=str(request.url),
         user=user,
     )
+
 
 def to_response(response: OAuthResponse) -> Response:
     """
@@ -57,13 +69,13 @@ def to_response(response: OAuthResponse) -> Response:
     return Response(
         content=json.dumps(response.content),
         headers=dict(response.headers),
-        status_code=response.status_code
+        status_code=response.status_code,
     )
 
-@app.get('/oauth/authorize')
+
+@app.get("/oauth/authorize")
 async def authorize(
-    request: Request,
-    oauth:   AuthServer = Depends(get_auth_server)
+    request: Request, oauth: AuthServer = Depends(get_auth_server)
 ) -> Response:
     """
     oauth2 authorization endpoint using aioauth
@@ -71,14 +83,15 @@ async def authorize(
     oauthreq = await to_request(request)
     response = await oauth.create_authorization_response(oauthreq)
     if response.status_code == HTTPStatus.UNAUTHORIZED:
-        request.session['oauth'] = oauthreq
-        return RedirectResponse('/login')
+        request.session["oauth"] = oauthreq
+        return RedirectResponse("/login")
     return to_response(response)
 
-@app.post('/oauth/tokenize')
+
+@app.post("/oauth/tokenize")
 async def tokenize(
     request: Request,
-    oauth:   AuthServer = Depends(get_auth_server),
+    oauth: AuthServer = Depends(get_auth_server),
 ):
     """
     oauth2 tokenization endpoint using aioauth
@@ -87,17 +100,21 @@ async def tokenize(
     response = await oauth.create_token_response(oauthreq)
     return to_response(response)
 
-@app.get('/login')
-async def login(
-    request: Request,
-    oauth:   AuthServer = Depends(get_auth_server)
-):
+
+@app.get("/login")
+async def login(request: Request, oauth: AuthServer = Depends(get_auth_server)):
     """
     barebones "login" page, redirected to when authorize is called before login
     """
     # sign in user
-    oauthreq = request.session['oauth']
+    oauthreq = request.session["oauth"]
     oauthreq.user = await auto_login()
     # process authorize request
     response = await oauth.create_authorization_response(oauthreq)
     return to_response(response)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
--- examples/shared/__init__.py
+++ examples/shared/__init__.py
@@ -1,29 +1,48 @@
 """
-Utilis and Implementation for AioAuth Storage Interfaces
+Shared Utilites and Implementation for AioAuth Storage Interfaces
 """
+
 from contextlib import asynccontextmanager
 
+import os
 from aioauth.server import AuthorizationServer
 
 from sqlmodel import SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
+from .config import load_config
 from .models import Client
 from .storage import BackendStore, User
 
-#** Variables **#
-__all__ = ['AuthServer', 'BackendStore', 'engine', 'auto_login', 'lifespan']
+__all__ = [
+    "AuthServer",
+    "BackendStore",
+    "engine",
+    "config",
+    "settings",
+    "auto_login",
+    "lifespan",
+]
 
-engine: AsyncEngine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=False, future=True)
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../config.json")
+
+engine: AsyncEngine = create_async_engine(
+    "sqlite+aiosqlite:///:memory:", echo=False, future=True
+)
+
+config = load_config(CONFIG_PATH)
+settings = config.settings
+
 
 async def auto_login() -> User:
     """
     return test user-account simulating login
     """
     async with AsyncSession(engine) as conn:
-        sql = select(User).where(User.username == 'test')
+        sql = select(User).where(User.username == "test")
         return (await conn.exec(sql)).one()
+
 
 @asynccontextmanager
 async def lifespan(*_):
@@ -37,16 +56,16 @@ async def lifespan(*_):
     # create test records
     async with AsyncSession(engine) as session:
         user = User(
-            username='test',
-            password='password',
+            username="test",
+            password="password",
         )
         client = Client(
-            client_id='test_client',
-            client_secret='password',
-            grant_types='authorization_code,refresh_token',
-            redirect_uris='http://localhost:3000/redirect',
-            response_types='code',
-            scope='email'
+            client_id="test_client",
+            client_secret="password",
+            grant_types="authorization_code,refresh_token",
+            redirect_uris="http://localhost:3000/redirect",
+            response_types="code",
+            scope="email",
         )
         session.add(user)
         session.add(client)
@@ -54,6 +73,7 @@ async def lifespan(*_):
     yield
     # close connections on app closure
     await engine.dispose()
+
 
 class AuthServer(AuthorizationServer[User]):
     pass
